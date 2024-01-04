a650c79eab231118546323bf29f25d156c25b564
zahid ansari <zahid776592@gmail.com>
2024-01-04 21:08:50 +0530

    myblog02: zahid's project

---
+++ src/main/java/com/myblog02/Myblog02Application.java
@@ -0,0 +1,13 @@
+package com.myblog02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myblog02/Myblog02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
